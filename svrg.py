8e8a1fe351c4a9eb5b89e925b0b735fbf323477c
Ryan Goldenberg <ryandgoldenberg1@gmail.com>
2019-11-07 22:07:25 -0500

    Added seed for reproducability.

--- svrg.py
+++ svrg.py
@@ -6,39 +6,17 @@ import torchvision
 from torchvision import datasets, transforms
 
 
-class SDGTrainer:
-    def __init__(self, model, loss_fn, optimizer):
-        self.model = model
-        self.loss_fn = loss_fn
-        self.optimizer = optimizer
-
-    def train(self, train_loader, num_epochs):
-        for epoch in range(1, num_epochs+1):
-            train_loss = 0
-            for batch in train_loader:
-                # Training Step
-                data, label = batch
-                self.optimizer.zero_grad()
-                prediction = self.model(data)
-                loss = self.loss_fn(prediction, label)
-                loss.backward()
-                self.optimizer.step()
-                # Update Statistics
-                train_loss += loss.item()
-            avg_train_loss = train_loss / len(train_loader)
-            print('[Epoch {}] train_loss: {:.02f}'.format(epoch, avg_train_loss))
-
-
 class SVRGTrainer:
     def __init__(self, create_model, loss_fn):
         self.create_model = create_model
         self.loss_fn = loss_fn
 
-    def train(self, train_loader, num_warmup_epochs, num_epochs, num_sub_epochs, learning_rate, weight_decay):
+    def train(self, train_loader, num_warmup_epochs, num_outer_epochs, num_inner_epochs, learning_rate):
         model = self.create_model()
         target_model = self.create_model()
 
-        warmup_optimizer = torch.optim.SGD(target_model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        # Perform several epochs of SGD as initialization for SVRG
+        warmup_optimizer = torch.optim.SGD(target_model.parameters(), lr=learning_rate)
         for warmup_epoch in range(1, num_warmup_epochs+1):
             warmup_loss = 0
             for batch in train_loader:
@@ -52,7 +30,7 @@ class SVRGTrainer:
             avg_warmup_loss = warmup_loss / len(train_loader)
             print('[Warmup {}/{}] loss: {:.02f}'.format(warmup_epoch, num_warmup_epochs, avg_warmup_loss))
 
-        for epoch in range(1, num_epochs+1):
+        for epoch in range(1, num_outer_epochs+1):
             # Find full target gradient
             target_model.zero_grad()
             for batch in train_loader:
@@ -69,10 +47,11 @@ class SVRGTrainer:
             # Initialize model to target model
             model.load_state_dict(copy.deepcopy(target_model.state_dict()))
 
-            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
             model_state_dicts = []
-            for sub_epoch in range(1, num_sub_epochs+1):
+            for sub_epoch in range(1, num_inner_epochs+1):
                 train_loss = 0
+                step = 0
                 for batch in train_loader:
                     data, label = batch
                     optimizer.zero_grad()
@@ -94,25 +73,28 @@ class SVRGTrainer:
                     train_loss += model_loss.item()
                     model_state_dicts.append(copy.deepcopy(model.state_dict()))
                 avg_train_loss = train_loss / len(train_loader)
-                print('[Outer {}/{}, Inner {}/{}] loss: {:.02f}'.format(epoch, num_epochs, sub_epoch, num_sub_epochs, avg_train_loss))
+                print('[Outer {}/{}, Inner {}/{}] loss: {:.03f}'.format(epoch, num_outer_epochs, sub_epoch, num_inner_epochs, avg_train_loss))
 
             new_target_state_dict = random.choice(model_state_dicts)
             target_model.load_state_dict(new_target_state_dict)
 
 
 def create_mlp():
-    return nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+    return nn.Sequential(nn.Flatten(), nn.Linear(784, 100), nn.ReLU(), nn.Linear(100, 10))
 
 
 
 if __name__ == '__main__':
     dataset_size = 600
     batch_size = 1
-    num_epochs = 10
-    num_sub_epochs = 2
+    num_outer_epochs = 10
+    num_inner_epochs = 2
     num_warmup_epochs = 3
-    learning_rate = 0.001
-    weight_decay = 0.001
+    learning_rate = 0.01
+    seed = 3
+
+    torch.manual_seed(seed)
+    random.seed(seed)
 
     train_ds = datasets.MNIST('~/datasets/pytorch', transform=transforms.ToTensor())
     train_ds = torch.utils.data.dataset.Subset(train_ds, indices=list(range(dataset_size)))
@@ -123,8 +105,7 @@ if __name__ == '__main__':
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-    # trainer = SDGTrainer(model=model, loss_fn=loss_fn, optimizer=optimizer)
-    # trainer.train(train_loader, num_epochs=num_epochs*10)
+
     trainer = SVRGTrainer(create_model=create_mlp, loss_fn=loss_fn)
-    trainer.train(train_loader, num_epochs=num_epochs, num_sub_epochs=num_sub_epochs,
-                  learning_rate=learning_rate, num_warmup_epochs=num_warmup_epochs, weight_decay=weight_decay)
+    trainer.train(train_loader, num_outer_epochs=num_outer_epochs, num_inner_epochs=num_inner_epochs,
+                  learning_rate=learning_rate, num_warmup_epochs=num_warmup_epochs)
